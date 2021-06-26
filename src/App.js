920bf3e6a422e9bbb829977e0ccd0e649ac66244
Anderson Fernandes <fernandesanderson14@gmail.com>
2021-06-25 23:03:25 -0300

    Added MUI dark theme
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 import Routes from './components/Routes'
 
 export default function App() {
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
+
   return (
-    <>
+    <ThemeProvider theme={ darkTheme }>
       <CssBaseline />
       <Routes />
-    </>
+    </ThemeProvider>
   );
 }
