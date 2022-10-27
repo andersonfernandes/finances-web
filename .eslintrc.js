a92a8e62d6d019081a9e93f863aa51737ccd20ce
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-10-26 22:49:13 -0300

    Add eslint
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	},
+	'settings': {
+		'react': {
+			'createClass': 'createReactClass',
+			'pragma': 'React',
+			'fragment': 'Fragment',
+			'version': 'detect',
+			'flowVersion': '0.53'
+		},
+		'propWrapperFunctions': [
+			'forbidExtraProps',
+			{'property': 'freeze', 'object': 'Object'},
+			{'property': 'myFavoriteWrapper'},
+			{'property': 'forbidExtraProps', 'exact': true}
+		],
+		'componentWrapperFunctions': [
+			'observer',
+			{'property': 'styled'},
+			{'property': 'observer', 'object': 'Mobx'},
+			{'property': 'observer', 'object': '<pragma>'}
+		],
+		'formComponents': [
+			'CustomForm',
+			{'name': 'Form', 'formAttribute': 'endpoint'}
+		],
+		'linkComponents': [
+			'Hyperlink',
+			{'name': 'Link', 'linkAttribute': 'to'}
+		]
+	}
+}
