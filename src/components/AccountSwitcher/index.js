6331dbe10711ebf39617cc83531730e5e411d373
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-10-27 23:38:58 -0300

    Add accounts load from api on AccountSwitcher
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- src/components/AccountSwitcher/index.js
+++ src/components/AccountSwitcher/index.js
@@ -1,31 +1,44 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
+
+import { List, ListItem, ListItemButton, ListItemText } from '@mui/material'
+
 import FormDialog from '../FormDialog'
-import {List, ListItem, ListItemButton, ListItemText} from '@mui/material'
+import AppContext from '../../context/AppContext'
+import AccountsService from '../../adapters/services/AccountsService'
 
 const AccountSwitcher = ({
   open,
   setOpen,
 }) => {
+  const [accounts, setAccounts] = useState([])
+
+  const { setLoading } = useContext(AppContext)
+
+  useEffect(() => {
+    setLoading(true)
+
+    AccountsService.loadAccounts()
+      .then(data => {
+        setAccounts(data)
+        setLoading(false)
+      })
+  }, [])
+
   return (
     <FormDialog
       open={open}
       setOpen={setOpen}
       title="Switch Account"
     >
-      {/* TODO: Fetch from API */}
       <List>
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemText primary="Account A" />
-          </ListItemButton>
-        </ListItem>
-
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemText primary="Account B" />
-          </ListItemButton>
-        </ListItem>
+        {accounts.map(account => (
+          <ListItem disablePadding key={account.name}>
+            <ListItemButton>
+              <ListItemText primary={account.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
       </List>
     </FormDialog>
   )
